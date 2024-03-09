39aff6d0edfecfd8cc3c4d89d102ae6c73935cee
Albert Valdes <albert.anthony1999@gmail.com>
2024-03-08 23:05:45 -0600

    wip: added styled dropdown component and create task form modal

--- Client/src/components/TaskCreationModal.tsx
+++ Client/src/components/TaskCreationModal.tsx
@@ -1,3 +1,5 @@
+import { useForm } from 'react-hook-form';
+import StyledDropdown from './micro/StyledDropdown';
 import IconClose from '../assets/icons/IconClose.svg?react';
 import './TaskCreationModal.scss';
 
@@ -6,22 +8,80 @@ interface Props {
 }
 
 export default function TaskCreationModal({ onStateChange }: Props) {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors }
+  } = useForm({
+    defaultValues: {
+      title: '',
+      description: '',
+      severity: { value: 'Low', label: 'Low', color: '#00ff66' },
+      startDate: '',
+      endDate: ''
+    }
+  });
+
+  const severityValue = watch('severity');
+
   return (
     <div className="task-creation-modal">
       <div className="form-container">
         <h2>Create new task</h2>
-        <p>Use this form to setup your new task</p>
+        <p className="form-note">Use this form to setup your new task</p>
         <IconClose onClick={() => onStateChange(false)} className="icon-close" />
-        <form>
+        <form
+          onSubmit={handleSubmit((data) => {
+            console.log(data);
+          })}
+        >
           <div className="input-container">
             <label htmlFor="title">Title</label>
-            <input id="title" type="text" placeholder="Enter title" />
-            <div className="caption">0/100</div>
+            <input
+              {...register('title', {
+                required: 'Task must have a title.',
+                minLength: { value: 4, message: 'Title must be at least 4 characters long.' }
+              })}
+              id="title"
+              type="text"
+              placeholder="Enter title"
+            />
+            <div className="input--helper">
+              <div className="caption text__error">{errors.title?.message}</div>
+              <div className="caption">0/100</div>
+            </div>
           </div>
           <div className="input-container">
             <label htmlFor="description">Description</label>
-            <input id="description" type="text" placeholder="Enter description" />
+            <input
+              {...register('description')}
+              id="description"
+              type="text"
+              placeholder="Enter description"
+            />
             <div className="caption">0/100</div>
+          </div>
+          <StyledDropdown
+            value={severityValue}
+            onChange={(selectedOption) => setValue('severity', selectedOption)}
+          />
+          <div className="dates-container">
+            <div className="input-container">
+              <label htmlFor="start-date">Start Date</label>
+              <input {...register('startDate')} id="start-date" type="date" />
+            </div>
+            <div className="input-container">
+              <label htmlFor="end-date">End Date</label>
+              <input {...register('endDate')} id="end-date" type="date" />
+            </div>
+          </div>
+          <div className="buttons-container">
+            <button type="button" onClick={() => onStateChange(false)} className="button__cancel">
+              Cancel
+            </button>
+            <button className="button__primary">Create Task</button>
           </div>
         </form>
       </div>
---
+++ Client/src/components/micro/StyledDropdown.tsx
@@ -0,0 +1,56 @@
+import Select, { StylesConfig } from 'react-select';
+import './StyledDropdown.scss';
+
+interface ColorOption {
+  readonly value: string;
+  readonly label: string;
+  readonly color: string;
+}
+
+const colorOptions: readonly ColorOption[] = [
+  { value: 'Trivial', label: 'Trivial', color: '#3bcbff ' },
+  { value: 'Low', label: 'Low', color: '#00ff66' },
+  { value: 'Medium', label: 'Medium', color: '#ffbb00' },
+  { value: 'High', label: 'High', color: '#f84c17' },
+  { value: 'Critical', label: 'Critical', color: '#dc143c' }
+];
+
+const dot = (color = 'transparent') => ({
+  alignItems: 'center',
+  display: 'flex',
+
+  ':before': {
+    backgroundColor: color,
+    borderRadius: 10,
+    content: '" "',
+    display: 'block',
+    marginRight: 8,
+    height: 10,
+    width: 10
+  }
+});
+
+const colorStyles: StylesConfig<ColorOption> = {
+  singleValue: (styles, { data }) => ({ ...styles, ...dot(data.color) })
+};
+
+interface Props {
+  value: ColorOption;
+  onChange: (value: ColorOption) => void;
+}
+
+export default function StyledDropdown({ value, onChange }: Props) {
+  return (
+    <div className="styled-dropdown">
+      <p className="react-select--label">Severity</p>
+      <Select
+        value={value}
+        onChange={(selectedOption) => onChange(selectedOption as ColorOption)}
+        defaultValue={colorOptions[2]}
+        options={colorOptions}
+        styles={colorStyles}
+        classNamePrefix="react-select"
+      />
+    </div>
+  );
+}
