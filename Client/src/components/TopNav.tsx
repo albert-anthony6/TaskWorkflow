8fe8ce9a774fdadc57b4b6da33a0976f545b61d2
Albert Valdes <albert.anthony1999@gmail.com>
2024-06-17 02:11:30 -0500

    feat: top nav component completed

--- Client/src/components/TopNav.tsx
+++ Client/src/components/TopNav.tsx
@@ -1,12 +1,75 @@
+import { useState } from 'react';
 import './TopNav.scss';
+import { NavLink } from 'react-router-dom';
+import { useAppSelector } from '../store/configureStore';
+import IconHome from '../assets/icons/icon_home.svg?react';
+import IconFolder from '../assets/icons/icon_folder.svg?react';
+import IconMessage from '../assets/icons/icon_message.svg?react';
+import IconPeople from '../assets/icons/icon_people.svg?react';
+import IconPerson from '../assets/icons/icon_person.svg?react';
+import IconSettings from '../assets/icons/icon_settings.svg?react';
 import IconMenu from '../assets/icons/icon_menu.svg?react';
 import IconLogo from '../assets/icons/icon_logo.svg?react';
+import IconClose from '../assets/icons/icon_close.svg?react';
 
 export default function TopNav() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { currentUser } = useAppSelector((state) => state.user);
+
   return (
-    <nav className="top-nav">
-      <IconMenu className="icon-menu" />
-      <IconLogo className="logo__white" />
-    </nav>
+    <div className="top-nav">
+      <div className="app-header">
+        <IconMenu onClick={() => setIsMenuOpen(!isMenuOpen)} className="icon-menu" />
+        <IconLogo className="logo__white" />
+      </div>
+      {isMenuOpen && (
+        <div className="modal-container">
+          <IconLogo onClick={() => setIsMenuOpen(!isMenuOpen)} className="logo__white" />
+          <IconClose onClick={() => setIsMenuOpen(false)} className="icon-close" />
+          <nav className="page-links">
+            <ul>
+              <li onClick={() => setIsMenuOpen(false)}>
+                <NavLink to={`/user/${currentUser?.id}`}>
+                  <IconHome />
+                  <p>Home</p>
+                </NavLink>
+              </li>
+              <li onClick={() => setIsMenuOpen(false)}>
+                <NavLink to="/projects">
+                  <IconFolder />
+                  <p>Projects</p>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/projects">
+                  <IconMessage />
+                  <p>Mentions</p>
+                  <div className="coming-soon-tag">Coming soon</div>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/people">
+                  <IconPeople />
+                  <p>Teams</p>
+                  <div className="coming-soon-tag">Coming soon</div>
+                </NavLink>
+              </li>
+              <li onClick={() => setIsMenuOpen(false)}>
+                <NavLink to="/people">
+                  <IconPerson />
+                  <p>People</p>
+                </NavLink>
+              </li>
+              <li onClick={() => setIsMenuOpen(false)}>
+                <NavLink to="/settings">
+                  <IconSettings />
+                  <p>Settings</p>
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      )}
+    </div>
   );
 }
