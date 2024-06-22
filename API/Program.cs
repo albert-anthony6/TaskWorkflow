d5448a8fc3dcf373518875b3dab6e4c19f93810c
Albert Valdes <albert.anthony1999@gmail.com>
2024-06-22 04:05:58 -0500

    wip: security headers added

--- API/Program.cs
+++ API/Program.cs
@@ -25,12 +25,34 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
 
+app.UseXContentTypeOptions();
+app.UseReferrerPolicy((opt) => opt.NoReferrer());
+app.UseXXssProtection((opt) => opt.EnabledWithBlockMode());
+app.UseXfo((opt) => opt.Deny());
+app.UseCsp((opt) => opt
+    .BlockAllMixedContent()
+    .StyleSources((s) => s.Self().CustomSources("https://fonts.googleapis.com"))
+    .FontSources((s) => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
+    .FormActions((s) => s.Self())
+    .FrameAncestors((s) => s.Self())
+    .ImageSources((s) => s.Self().CustomSources("blob:", "https://res.cloudinary.com"))
+    .ScriptSources((s) => s.Self())
+);
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI((c) => 
     {
         c.ConfigObject.AdditionalItems.Add("persistAuthorization", "true");
+    });
+}
+else
+{
+    app.Use(async (context, next) => 
+    {
+        context.Response.Headers.Append("Strict-Transport-Security", "max-age=31536000");
+        await next.Invoke();
     });
 }
 
