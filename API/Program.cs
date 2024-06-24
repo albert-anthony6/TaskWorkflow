ccd8f4df77614b0614c41ac9e75dbf78efe05164
Albert Valdes <albert.anthony1999@gmail.com>
2024-06-24 11:39:25 -0500

    wip: temporary update for csp

--- API/Program.cs
+++ API/Program.cs
@@ -31,7 +31,7 @@ app.UseXXssProtection((opt) => opt.EnabledWithBlockMode());
 app.UseXfo((opt) => opt.Deny());
 app.UseCsp((opt) => opt
     .BlockAllMixedContent()
-    .StyleSources((s) => s.Self().CustomSources("https://fonts.googleapis.com"))
+    .StyleSources((s) => s.Self().UnsafeInline().CustomSources("https://fonts.googleapis.com"))
     .FontSources((s) => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
     .FormActions((s) => s.Self())
     .FrameAncestors((s) => s.Self())
